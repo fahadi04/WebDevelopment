bd1cf128b769bd9925630c68db11902759c44f13
Fahad Masroor <119754253+fahadi04@users.noreply.github.com>
2024-01-24 01:27:12 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,41 @@
 let btn = document.querySelector("button");
+let ul = document.querySelector("ul");
+let inp = document.querySelector("input");
 
+// Addition of task is going on
 btn.addEventListener("click", function () {
-    let h3 = document.querySelector("h3");
-    let randomColor = getRandomColor();
-    h3.innerText = randomColor;
+    let item = document.createElement("li");
+    item.innerText = inp.value;
 
-    let div = document.querySelector("div");
-    div.style.backgroundColor = randomColor;
-    console.log("Colors updated");
+
+    let dltbtn = document.createElement("button");
+    dltbtn.innerText = "Delete";
+    dltbtn.classList.add("delete");
+    
+
+    item.appendChild(dltbtn);
+    ul.appendChild(item);
+    inp.value = "";
 
 });
 
-function getRandomColor() {
-    let red = Math.floor(Math.random() * 255);
-    let green = Math.floor(Math.random() * 255);
-    let blue = Math.floor(Math.random() * 255);
+// This is used for deleting the existing value in HTML only
 
-    let color = `rgb(${red},${green},${blue})`;
-    return color;
-}
+// let dltbtns = document.querySelectorAll(".delete");
+// for (dltbtn of dltbtns) {
+//     dltbtn.addEventListener("click", function () {
+//         let par = this.parentElement;
+//         console.log(par);
+//         par.remove();
+//     });
+// }
 
+
+//  Using Event Delegation Deletion of task is performing
+ul.addEventListener("click", function (event) {
+    if (event.target.nodeName == "BUTTON") {
+        let listItem = event.target.parentElement;
+        listItem.remove();  /// Navigation
+        console.log("deleted");
+    }
+});
